6725ea2361f304da05e1a5500433dc09fc509edc
C0D1NGD0J0 <web-dev@United4Life.local>
2017-05-30 16:42:32 +0100

    implemented ajax requests + refactored routing coding

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,16 +5,31 @@ router.get('/', (req, res) =>{
 	res.render('pages/index', {title: 'Contactlist App'});
 });
 
-router.get('/api/member', (req, res) =>{
-	res.send('Member index page');
+router.get('/contact', (req, res) =>{
+	res.send(data);
 });
 
-router.get('/api/member/profile', (req, res) =>{
-	res.send('Member profile page');
+router.get('/contact/:id', (req, res) =>{
+	const data = {
+		contactId: 1,
+		firstName: 'John',
+		lastName: 'Micheal',
+		email: 'john@micheal.com',
+		phone: '987234133'
+	}
+	res.send(data);
 });
 
-router.get('/api/admin', (req, res) =>{
-	res.send('Admin Page');
+router.post('/contact', (req, res) =>{
+	res.send(req.body);
+});
+
+router.put('/contact/:id', (req, res) =>{
+	res.send(req.body);
+});
+
+router.delete('/contact/:id', (req, res) =>{
+	res.send({message: 'Record deleted.'});
 });
 
 router.get('*', (req, res) =>{
---
+++ public/js/main.js
@@ -0,0 +1,90 @@
+function get(){
+	$.ajax({
+		type: 'GET',
+		url: 'http://localhost:3000/contact',
+		dataType: 'json'
+	})
+	.done((data) =>{
+		console.log('GET response:', JSON.stringify(data, '', 2));
+
+		$('#dataContactId').html(data.contactId);
+		$('#dataF_Name').html(data.firstName);
+		$('#dataL_Name').html(data.lastName);
+		$('#dataEmail').html(data.email);
+		$('#dataPhone').html(data.phone);
+	})
+	.fail((jqXHR, textStatus, err) =>{
+		console.log('Ajax error response:', textStatus);
+	});
+}
+
+function post(){
+	$.ajax({
+		type: 'POST',
+		url: 'http://localhost:3000/contact',
+		crossDomain: true,
+		contentType: 'application/json; charset=utf',
+		dataType: 'json',
+		data: JSON.stringify({
+			contactId: 2,
+			firstName: 'Chris',
+			lastName: 'Brown',
+			email: 'chris@brown.com',
+			phone: '22233344'
+		})
+	})
+	.done((data) =>{
+		console.log('POST response:', JSON.stringify(data, '', 2));
+		$('#dataContactId').html(data.contactId);
+		$('#dataF_Name').html(data.firstName);
+		$('#dataL_Name').html(data.lastName);
+		$('#dataEmail').html(data.email);
+		$('#dataPhone').html(data.phone);
+	})
+	.fail((jqXHR, textStatus, err) =>{
+		console.log('Ajax error response:', textStatus);
+	});
+}
+
+function put(){
+	$.ajax({
+		type: 'POST',
+		url: 'http://localhost:3000/contact/2',
+		crossDomain: true,
+		contentType: 'application/json; charset=utf',
+		dataType: 'json',
+		data: JSON.stringify({
+			contactId: 2,
+			firstName: 'Chris',
+			lastName: 'Brown',
+			email: 'chris@brown.com',
+			phone: '11155566'
+		})
+	})
+	.done((data) =>{
+		console.log('PUT response:', JSON.stringify(data, '', 2));
+		$('#dataContactId').html(data.contactId);
+		$('#dataF_Name').html(data.firstName);
+		$('#dataL_Name').html(data.lastName);
+		$('#dataEmail').html(data.email);
+		$('#dataPhone').html(data.phone);
+	})
+	.fail((jqXHR, textStatus, err) =>{
+		console.log('Ajax error response:', textStatus);
+	});
+}
+
+function del(){
+	$.ajax({
+		type: 'DELETE',
+		url: 'http://localhost:3000/contact/2',
+		dataType: 'json',
+	})
+	.done((data) =>{
+		console.log('DELETE response:', JSON.stringify(data, '', 2));
+		$('#getResponse').html(JSON.stringify(data, '', 2));
+	})
+	.fail((jqXHR, textStatus, err) =>{
+		console.log('Ajax error response:', textStatus);
+	});
+}
