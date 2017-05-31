0b44ee030e76a74de67a186f4119626569f5cf9e
C0D1NGD0J0 <web-dev@United4Life.local>
2017-05-31 17:55:00 +0100

    created post/get contacts route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,19 +1,33 @@
 'use strict';
 let router = require('express').Router();
-let {db} = require('./app/db/index');
-let {UserModel} = require('./app/model/user');
-let {ContactModel} = require('./app/model/contact');
+let {db} = require('../db/index');
+let {User} = require('../model/user');
+let {Contact} = require('../model/contact');
 
 router.get('/', (req, res) =>{
 	res.render('pages/index', {title: 'Contactlist App'});
 });
 
+router.get('/contacts', (req, res) => {
+	Contact.find().then((contacts) => {
+		res.send({contacts});
+	}, (e) => {
+		res.status(400).send(e);
+	});
+});
+
 router.post('/contacts', (req, res) =>{
-	let contact = new ContactModel({
+	let contact = new Contact({
 		firstName: 'Elizabeth',
 		lastName: 'Dolittle',
 		email: 'elizabeth@example.com',
 		phone: '08032145589'
+	});
+
+	contact.save().then((contact) => {
+		res.send(contact);
+	}, (e) => {
+		res.status(400).send(e);
 	});
 });
 
