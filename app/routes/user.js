02288ba836f518ee03a2a9457a3e668b689fe6d7
C0D1NGD0J0 <web-dev@United4Life.local>
2017-06-02 09:03:38 +0100

    commented out user.js route file

---
+++ app/routes/user.js
@@ -0,0 +1,11 @@
+// 'use strict';
+// const router = require('express').Router();
+// const {User} = require('../model/user');
+// const {ObjectID} = require('mongodb');
+// const _ = require('lodash');
+
+
+
+// router.get('*', (req, res) =>{
+// 	res.status(404).send('Page not found, enter a valid url.');
+// });
