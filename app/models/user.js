e5a7a03ab52cfc7dc7b472ea20483e05b2a6fadf
C0D1NGD0J0 <web-dev@United4Life.local>
2017-06-05 11:49:18 +0100

    implemented password hashing

--- app/models/user.js
+++ app/models/user.js
@@ -3,6 +3,7 @@ let mongoose = require('mongoose');
 let validator = require('validator');
 const jwt = require('jsonwebtoken');
 let _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 let UserSchema = new mongoose.Schema({
 	email: {
@@ -82,6 +83,20 @@ UserSchema.statics.findByToken = function(token) {
 		'tokens.access': 'auth'
 	});
 };
+
+UserSchema.pre('save', function(next){
+	let user = this;
+	if(user.isModified('password')){
+		bcrypt.genSalt(10, (err, salt) => {
+			bcrypt.hash(user.password, salt, (err, hash) => {
+				user.password = hash;
+				next();
+			});
+		});
+	} else {
+		next();
+	}
+});
 
 
 let User = mongoose.model('User', UserSchema);
